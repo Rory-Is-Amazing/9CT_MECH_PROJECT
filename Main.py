180929c822919cd7ae213d47a8e663b923d6068d
Rory-Is-Amazing <rory.tomkins@education.nsw.gov.au>
2026-08-13 15:47:20 +1000

    Completed Code & Switch
    Pretty much finnished code, bar notes and some tweaks, inclusion of an on/off switch

--- Main.py
+++ Main.py
@@ -4,78 +4,63 @@ from machine import Pin
 
 speaker = machine.PWM(machine.Pin(6))
 
-ts = 0
-
 ledr = Pin(12, Pin.OUT)
 ledy = Pin(13, Pin.OUT)
 ledg = Pin(14, Pin.OUT)
 ledb = Pin(15, Pin.OUT)
 
-ts1 = Pin(16, Pin.IN, Pin.PULL_DOWN)
-ts2 = Pin(17, Pin.IN, Pin.PULL_DOWN)
-ts3 = Pin(18, Pin.IN, Pin.PULL_DOWN)
+ts1 = Pin(17, Pin.IN, Pin.PULL_DOWN)
+ts2 = Pin(18, Pin.IN, Pin.PULL_DOWN)
+ts3 = Pin(20, Pin.IN, Pin.PULL_DOWN)
 
+sw = Pin(27, Pin.IN, Pin.PULL_UP)
+
+def play_tone(freq, dur):
+    if freq == 0:
+        speaker.duty_u16(0)
+    else:
+        speaker.freq(freq)
+        speaker.duty_u16(32768)
+    time.sleep(dur)
+    speaker.duty_u16(0)
 def main():
     while True:
-        
-        ts = ts1.value() + ts2.value() + ts3.value()
-        
-        ledr.value(0)
-        ledy.value(0)
-        ledg.value(0)
-        ledb.value(0)
-        if ts == 0:
-            red()
-        elif ts == 1:
-            yellow()
-        elif ts == 2:
-            green()
-        elif ts == 3:
-            blue()
+        if sw.value() == 0:
+            ts = ts1.value() + ts2.value() + ts3.value()
+
+            ledr.value(0)
+            ledy.value(0)
+            ledg.value(0)
+            ledb.value(0)
+
+            if ts == 0:
+                ledr.value(1)
+                play_tone(1000, 0.25)
+                time.sleep(0.25)
+
+            elif ts == 1:
+                ledy.value(1)
+                play_tone(1000, 0.125)
+                time.sleep(0.125)
+
+            elif ts == 2:
+                ledg.value(1)
+                play_tone(1000, 0.0625)
+                time.sleep(0.0625)
+
+            elif ts == 3:
+                ledb.value(1)
+                play_tone(1000, 0.03125)
+                time.sleep(0.03125)
+            else:
+                print("Error: Invalid tilt sensor value")
+        elif sw.value() == 1:
+            ledr.value(0)
+            ledy.value(0)
+            ledg.value(0)
+            ledb.value(0)
         else:
-            print("Error.")
-            break
-
-def play_tone(frequency, duration):
-    if frequency == 0:
-        speaker.duty_u16(0) # Turn off buzzer
-    else:
-        speaker.freq(frequency) 
-        speaker.duty_u16(32768) # Set buzzer volume to 50%
-        
-    time.sleep(duration)
-    speaker.duty_u16(0) # Turn off buzzer
-
-def red():
-    print()
-    time.sleep(0.25)
-    ledr.value(1)
-    play_tone(1000,0.25)
-    time.sleep(0.25)
-def yellow():
-    print('yellow')
-    time.sleep(0.125)
-    ledy.value(1)
-    play_tone(1000,0.125)
-    time.sleep(0.125)
-def green():
-    print('green')
-    time.sleep(0.0625)
-    ledg.value(1)
-    play_tone(1000,0.0625)
-    time.sleep(0.0625)
-def blue():
-    print('blue')
-    time.sleep(0.03125)
-    ledb.value(1)
-    play_tone(1000,0.03125)
-    time.sleep(0.03125)
-'''
-while True:
-    time.sleep(0.5)
-    play_tone(3000,0.5)
-'''
-
+            print("Error: Invalid switch value")
 
 if __name__ == "__main__":
     main()
---
+++ Tilt_Switch_Checker.py
@@ -0,0 +1,22 @@
+import machine
+import time
+from machine import Pin
+
+ts1 = Pin(17, Pin.IN, Pin.PULL_DOWN)
+ts2 = Pin(18, Pin.IN, Pin.PULL_DOWN)
+ts3 = Pin(20, Pin.IN, Pin.PULL_DOWN)
+
+def stable_read(pin):
+    samples = []
+    for _ in range(5):          # take 5 quick samples
+        samples.append(pin.value())
+        time.sleep(0.002)       # 2ms between samples
+    return 1 if sum(samples) >= 3 else 0   # majority vote
+
+while True:
+    sts1 = stable_read(ts1)
+    sts2 = stable_read(ts2)
+    sts3 = stable_read(ts3)
+
+    print(sts1, sts2, sts3, sts1 + sts2 + sts3)
+    time.sleep(0.05)
