932235ded1c6f4477d4b1f7a9394d5200a6d9dbd
Rory-Is-Amazing <rory.tomkins@education.nsw.gov.au>
2026-08-14 17:50:00 +1000

    Reorganised Versions, Main file fixed.

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import machine
 import time
 from machine import Pin
 
+speaker = machine.PWM(machine.Pin(6))
+
 ledr = Pin(12, Pin.OUT)
 ledy = Pin(13, Pin.OUT)
 ledg = Pin(14, Pin.OUT)
@@ -24,38 +26,52 @@ def play_tone(freq, dur):
         speaker.duty_u16(32768)
     time.sleep(dur)
     speaker.duty_u16(0)
+
 def main():
     while True:
-            ts = ts1.value() + ts2.value() + ts3.value()
+        ts = 0
+        
+        ledr.value(0)
+        ledy.value(0)
+        ledg.value(0)
+        ledb.value(0)
 
-            ledr.value(0)
-            ledy.value(0)
-            ledg.value(0)
-            ledb.value(0)
+        ts = ts1.value() + ts2.value() + ts3.value()
+        if ts == 0:
+            red()
+        elif ts == 1:
+            yellow()
+        elif ts == 2:
+            green()
+        elif ts == 3:
+            blue()
+        else:
+            print("Error: Invalid tilt sensor value")
 
-            if ts == 0:
-                ledr.value(1)
-                play_tone(1000, 0.25)
-                time.sleep(0.25)
+def red():
+    ledr.value(1)
+    play_tone(1000, 0.25)
+    time.sleep(0.25)
+    ledr.value(0)
 
-            elif ts == 1:
-                ledy.value(1)
-                play_tone(1000, 0.125)
-                time.sleep(0.125)
+def yellow():
+    ledy.value(1)
+    play_tone(1000, 0.125)
+    time.sleep(0.125)
+    ledy.value(0)
 
-            elif ts == 2:
-                ledg.value(1)
-                play_tone(1000, 0.0625)
-                time.sleep(0.0625)
+def green():
+    ledg.value(1)
+    play_tone(1000, 0.0625)
+    time.sleep(0.0625)
+    ledg.value(0)   
 
-            elif ts == 3:
-                ledb.value(1)
-                play_tone(1000, 0.03125)
-                time.sleep(0.03125)
-            else:
-                print("Error: Invalid tilt sensor value")
-
-
+def blue():
+    ledb.value(1)
+    play_tone(1000, 0.03125)
+    time.sleep(0.03125)
+    ledb.value(0)
+    
 if __name__ == "__main__":
     main()
 
